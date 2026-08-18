23015a93c0d74b5b70bf1830d104865857fdea74
Heber <suarezheber@gmail.com>
2026-08-18 11:41:07 -0300

    comienzo js tienda

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 import './scss/app.scss'
 import './images'
+
+const productos = [
+    {
+        id: "mascarilla-01",
+        titulo: "Mascarilla",
+        imagen: "/src/img/01.webp",
+        categoria: {
+            nombre: "mascarilla",
+            id: "mascarilla"
+        },
+        precio: 1000
+    } 
+
+]
